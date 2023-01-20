4b10471e5f73e63040bbcb50fca6bfd34c0027ae
Jake Cattrall <krazyjakee@gmail.com>
2023-01-20 21:36:11 +0000

    better handling of default values and remove unsupported types

--- entities/other/script_only.gd
+++ entities/other/script_only.gd
@@ -1,6 +1,6 @@
 extends Node3D
 
-@export var _bool: bool = true
+@export var _bool = true
 @export var _string = "string"
 @export var _int = 1
 @export var _float = 2.0
@@ -12,3 +12,13 @@ extends Node3D
 @export var b = 1
 @export var c = 1
 @export var fgd_block = ["a", "b"]
+
+@export var _bool_nd: bool
+@export var _string_nd: String
+@export var _int_nd: int
+@export var _float_nd: float
+@export var _color_nd: Color
+@export var _choice_nd: Array
+@export var _flags_nd: Dictionary
+
+@export var unsupported_node: Node
